0bc0327ecf0c8802e73c137fb9402ee432e3f301
Vinnyda23 <vinnycontateme@gmail.com>
2026-08-17 18:47:00 -0300

    Feat/PgListagem,AddLivro

--- src/app/components/livros/livroslist/livroslist.component.ts
+++ src/app/components/livros/livroslist/livroslist.component.ts
@@ -12,7 +12,7 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./livroslist.component.scss']
 })
 export class LivroslistComponent {
-
+  
   lista: LivroModel[] = [
   new LivroModel(1, 'Dom Casmurro', 'Machado de Assis', 'Editora Ática', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTekkRUyu7uOF8FEmpam0XG0AhQZLESlkb9Q57R_yfIUg&s'),
   new LivroModel(2, '1984', 'George Orwell', 'Companhia das Letras', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNJmuJqAPa3P4-vVoK0IdEskS5VrNEPVgFdtHJ5zHO_Q&s'),
@@ -20,4 +20,19 @@ export class LivroslistComponent {
   new LivroModel(4, 'Cem Anos de Solidão', 'Gabriel García Márquez', 'Editora Record', 'https://placehold.co/45x45/dc3545/white?text=Chttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQTgBdo5gSmw-aAVRZ-IbrffCTF4zS2NcbgifyuKZxoQ&s=10A')
   ];
 
+  constructor() {
+    const livroNovo = history.state.livroNovo;
+    const livroEditado = history.state.livroEditado;
+
+    if (livroEditado) {
+      const indice = this.lista.findIndex(x => x.id == livroEditado.id);
+      this.lista[indice] = livroEditado;
+    }
+
+    if (livroNovo) {
+      livroNovo.id = 555;
+      this.lista.push(livroNovo);
+    }
   }
+
+}
