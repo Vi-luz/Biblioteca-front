5ccac2ce96f032f3cf3025d99671668d17b90596
Vi-luz <victorluisdaluz57@gmail.com>
2026-08-16 22:42:26 -0300

    Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import { LivroslistComponent } from './components/livroslist/livroslist.component';
+import { LoginComponent } from './components/layout/login/login.component';
+import { PrincipalComponent } from './components/layout/principal/principal.component';
+import { LivroslistComponent } from './components/livros/livroslist/livroslist.component';
+import { LivrosdetailsComponent } from './components/livros/livrosdetails/livrosdetails.component';
 
 export const routes: Routes = [
-    [path: "livros", Component: LivroslistComponent]
+    { path: "", redirectTo: "login", pathMatch: "full" },
+    { path: "login", component: LoginComponent },
+    { path: "admin", component: PrincipalComponent, children: [
+        { path: "livros", component: LivroslistComponent },
+        { path: "livros/new", component: LivrosdetailsComponent },
+        { path: "livros/edit/:id", component: LivrosdetailsComponent}
+    ]}
 ];
